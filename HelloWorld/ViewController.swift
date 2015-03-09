4b2ef55ddba0e84bcf11e76decd044f8d63e7e34
lyndseylou <lyndsey.meadows@hotmail.com>
2015-03-09 07:56:19 -0500

    i added files

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -8,17 +8,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+class ViewController: UIViewController, UITableViewDelegate {
+    @IBOutlet var appsTableView : UITableView?
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section:    Int) -> Int {
+        return 10
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "MyTestCell")
+        
+        cell.textLabel?.text = "Row #\(indexPath.row)"
+        cell.detailTextLabel?.text = "Subtitle #\(indexPath.row)"
+        
+        return cell
     }
+    
 
 
 }
